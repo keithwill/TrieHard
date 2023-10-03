76af5c8c0e63d2618afcdc59d88ec304b5537c81
Keith Williams <keith.williams.dev@gmail.com>
2023-10-03 15:13:04 -0400

    Stripped most of the abstractions out of the ArrayPoolList.

--- src/TrieHard.PrefixLookup/ArrayPoolList.cs
+++ src/TrieHard.PrefixLookup/ArrayPoolList.cs
@@ -1,60 +1,32 @@
 ﻿using System.Buffers;
-using System.Collections;
-using System.Runtime.CompilerServices;
-using System.Threading.Channels;
-using System.Xml.Linq;
+using System.Collections.Concurrent;
 
 namespace TrieHard.Collections
 {
-    internal class ArrayPoolList<T> : IList<T>, IDisposable
+    /// <summary>
+    /// This was an experiment taken from a dotnet GitHub issue where they were exploring
+    /// the use of a pooled list class for the BCL. I pilfered it, but added some rent
+    /// and return methods to also reused the list object itself and stripped down some of the
+    /// code that implemented IList<typeparamref name="T"/>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class ArrayPoolList<T> : IDisposable
     {
-        
-        internal static ArrayPoolList<T> Rent()
-        {
-            if (Pool.Reader.TryRead(out var arrayList))
-            {
-                return arrayList;
-            }
-            return new ArrayPoolList<T>();
-        }
-
-        internal static void Return(ArrayPoolList<T> arrayList)
-        {
-            arrayList.Clear();
-            Array.Clear(arrayList.Items);
-            Pool.Writer.TryWrite(arrayList);
-        }
-
-        internal static Channel<ArrayPoolList<T>> Pool =
-            Channel.CreateBounded<ArrayPoolList<T>>(
-            new BoundedChannelOptions(10000)
-            {
-                FullMode = BoundedChannelFullMode.DropNewest
-            }, x => { x.Dispose(); }
-        );
-
         private T[] _items;
-        private int _size, _version;
-        public ArrayPoolList()
-            => _items = Array.Empty<T>();
+        private int _size;
+
+        public ArrayPoolList() => _items = Array.Empty<T>();
+        public ArrayPoolList(int capacity) => _items = capacity == 0 ?
+            Array.Empty<T>() : 
+            ArrayPool<T>.Shared.Rent(capacity);
 
         public ref T[] Items => ref _items;
-
-        public ArrayPoolList(int capacity)
-            // note: can probably just use  => _items = ArrayPool<T>.Shared.Rent(capacity);
-            => _items = capacity == 0 ? Array.Empty<T>() : ArrayPool<T>.Shared.Rent(capacity);
-
         public int Count => _size;
-
         public Span<T> Span => _items.AsSpan(0, _size);
 
-        public bool IsReadOnly => false;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int Add(T item)
         {
-            _version++;
-            var array = DisposeCheck(_items);
+            var array = _items;
             int size = _size;
             if ((uint)size < (uint)array.Length)
             {
@@ -68,14 +40,6 @@ namespace TrieHard.Collections
             }
         }
 
-        public void Dispose()
-        {
-            var arr = _items;
-            _items = null!;
-            if (arr != null) ArrayPool<T>.Shared.Return(arr);
-        }
-
-        [MethodImpl(MethodImplOptions.NoInlining)]
         private int AddWithResize(T item)
         {
             int size = _size;
@@ -98,24 +62,11 @@ namespace TrieHard.Collections
             }
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        static T[] ThrowObjectDisposedException()
-        {
-            ThrowHelper.ThrowObjectDisposedException(nameof(ArrayPoolList<T>));
-            return null; // just to make compiler happy
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static T[] DisposeCheck(T[] array)
-            => array ?? ThrowObjectDisposedException();
-
         public int Capacity
         {
-            get => DisposeCheck(_items).Length;
+            get => _items.Length;
             set
             {
-                if (value < _size) ThrowHelper.ThrowArgumentOutOfRangeException(nameof(Capacity));
-
                 if (value != _items.Length)
                 {
                     if (value > 0)
@@ -140,68 +91,42 @@ namespace TrieHard.Collections
 
         public T this[int index]
         {
-            get
-            {
-                if ((uint)index >= (uint)_size) ThrowHelper.ThrowIndexOutOfRangeException();
-                return DisposeCheck(_items)[index];
-            }
-
-            set
-            {
-                if ((uint)index >= (uint)_size) ThrowHelper.ThrowIndexOutOfRangeException();
-                _version++;
-                DisposeCheck(_items)[index] = value;
-            }
+            get => _items[index];
+            set => _items[index] = value;
         }
 
         public void Clear()
         {
-            _version++;
             _size = 0;
         }
 
-        public bool Contains(T item)
-            => _size != 0 && IndexOf(item) != -1;
-
-        public int IndexOf(T item)
-            => Array.IndexOf(DisposeCheck(_items), item, 0, _size);
-
-        public void CopyTo(T[] array, int arrayIndex)
-            => Array.Copy(DisposeCheck(_items), 0, array, arrayIndex, _size);
-
-        public IEnumerator<T> GetEnumerator()
-            => throw new NotImplementedException();
-
-        public bool Remove(T item)
-            => throw new NotImplementedException();
-
-        IEnumerator IEnumerable.GetEnumerator()
-            => throw new NotImplementedException();
-
-        public void Insert(int index, T item)
-            => throw new NotImplementedException();
-
-        public void RemoveAt(int index)
-            => throw new NotImplementedException();
-
-        void ICollection<T>.Add(T item)
+        internal static ArrayPoolList<T> Rent()
         {
-            this.Add(item);
+            if (Pool.TryDequeue(out var arrayList))
+            {
+                return arrayList;
+            }
+            return new ArrayPoolList<T>();
         }
+
+        internal static void Return(ArrayPoolList<T> arrayList)
+        {
+            arrayList.Clear();
+            Array.Clear(arrayList.Items);
+            Pool.Enqueue(arrayList);
+        }
+        
+        internal static readonly ConcurrentQueue<ArrayPoolList<T>> Pool = new ConcurrentQueue<ArrayPoolList<T>>();
+
+        public void Dispose()
+        {
+            var arr = _items;
+            _items = null!;
+            if (arr != null) ArrayPool<T>.Shared.Return(arr);
+        }
+
     }
 
-    static class ThrowHelper
-    {
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public static void ThrowArgumentOutOfRangeException(string paramName)
-            => throw new ArgumentOutOfRangeException(paramName);
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public static void ThrowIndexOutOfRangeException()
-            => throw new IndexOutOfRangeException();
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public static void ThrowObjectDisposedException(string objectName)
-            => throw new ObjectDisposedException(objectName);
-    }
 }
 
 
