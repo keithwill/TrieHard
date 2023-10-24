fd38d568d66b0b51c64281293d625a80843ab390
Keith Williams <keith.williams.dev@gmail.com>
2023-10-24 10:48:00 -0400

    Removed some dead code. While considering if the SpanExentsionMethods.cs file could be removed, noticed that inlining the CopyWithInsert method improved its performance noticably.

--- src/TrieHard.PrefixLookup/SpanExtensionMethods.cs
+++ src/TrieHard.PrefixLookup/SpanExtensionMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace TrieHard.PrefixLookup
 {
     internal static class SpanExtensionMethods
     {
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void CopyWithInsert<T>(this Span<T> source, Span<T> destination, T insertValue, int atIndex)
         {
 
--- src/TrieHard.PrefixLookup/Unsafe/HybridStackInlineArray.cs
+++
@@ -1,6 +0,0 @@
-[System.Runtime.CompilerServices.InlineArray(Size)]
-public struct HybridStackInlineArray<T> where T : unmanaged
-{
-    public const int Size = 36;
-    private T _element0;
-}
